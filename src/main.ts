ae1a0c0d41bccadbf7f9e38a80a94db45581d233
jose.correa <jose.correa@e-unicred.com.br>
2025-05-29 10:38:30 -0300

    other commit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // ou o domínio do seu frontend
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
